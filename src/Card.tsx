f16eeab02d91fe4387872fac4b105c1ddf3ff515
Moises Man <mcarlosman@gmail.com>
2024-11-10 08:26:27 -0500

    cleaning

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,6 +1,6 @@
 import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
-import { draugMage, whoCanIAttack } from './monsters'
+import { whoCanIAttack } from './monsters'
 import { useStackStore } from './stackStore'
 import { Stack } from './types'
 
