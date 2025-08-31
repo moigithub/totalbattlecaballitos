7d846b36a9e1b5008567c65e585974be1002f594
Moises Man <mcarlosman@gmail.com>
2025-08-31 09:03:49 -0500

    add label catapult calc

--- src/citadels.tsx
+++ src/citadels.tsx
@@ -434,14 +434,17 @@ export const Citadels = () => {
                       onChange={changeHPBonus}
                     />
                   </div>
-                  <input
-                    type='number'
-                    className='ml-1 inline-flex bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-0.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                    value={cataMaxHealth}
-                    onChange={e => {
-                      setCitadelCataMaxHealth(parseInt(e.target.value))
-                    }}
-                  />{' '}
+                  <div className='group'>
+                    <label>HP max limit </label>
+                    <input
+                      type='number'
+                      className='ml-1 inline-flex bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-0.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                      value={cataMaxHealth}
+                      onChange={e => {
+                        setCitadelCataMaxHealth(parseInt(e.target.value))
+                      }}
+                    />
+                  </div>
                 </>
               )}
             </div>
